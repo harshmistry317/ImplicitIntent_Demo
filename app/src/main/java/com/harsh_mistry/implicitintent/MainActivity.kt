ca941b726701fa8ca72eb12c53617dc9c034b52d
harsh317 <mistryhk317@gmail.com>
2022-12-29 15:07:17 +0530

    Main file code added

--- app/src/main/java/com/harsh_mistry/implicitintent/MainActivity.kt
+++ app/src/main/java/com/harsh_mistry/implicitintent/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.harsh_mistry.implicitintent
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_call.setOnClickListener {
+            var intent= Intent()
+            intent.setAction(Intent.ACTION_DIAL)
+            intent.setData(Uri.parse("tel:"+et_call.text.toString()))
+            startActivity(intent)
+        }
+
     }
 }
